b0736569e3ddaaa1ad78e6e1aa44deee171a434b
André <AndreR@ppedv.de>
2024-01-15 13:24:15 +0100

    Hallo WCF mit VS Templates

---
+++ HalloWCF/HalloWCF.Client_Framework/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloWCF.Client_Framework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** WCF Client ***");
+
+            var ser1 = new ServiceReference1.Service1Client();
+
+            var result = ser1.GetData(12);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HalloWCF/HalloWCF.Client_net8/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+var client = new ServiceReference1.Service1Client();
+
+var result = client.GetDataAsync(12).Result;
+Console.WriteLine(result);
